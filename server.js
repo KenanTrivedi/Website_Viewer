87a92d1a54b96222d422f390c85717773cec9eea
KenanTrivedi <kenanlion4@gmail.com>
2024-05-29 02:31:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 
 const app = express()
@@ -14,17 +15,30 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB...', err))
 
-const User = mongoose.model(
-  'User',
-  new mongoose.Schema({
-    username: String,
-    password: String,
-    surveyResponses: [String],
-  })
-)
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  surveyResponses: [String],
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+const User = mongoose.model('User', UserSchema)
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const newUser = new User({ username, password: hashedPassword })
+  await newUser.save()
+  res.status(201).send('User registered successfully')
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  const user = await User.findOne({ username })
+  if (!user) return res.status(400).send('Invalid username or password')
+  const validPassword = await bcrypt.compare(password, user.password)
+  if (!validPassword)
+    return res.status(400).send('Invalid username or password')
+  res.send('Login successful')
 })
 
 app.listen(3000, () => {
