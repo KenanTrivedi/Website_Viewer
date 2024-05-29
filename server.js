d6999c0e36b687f4081093bfdb009989080cd3e5
KenanTrivedi <kenanlion4@gmail.com>
2024-05-29 02:20:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ require('dotenv').config()
 const app = express()
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err))
 
 const User = mongoose.model(
   'User',
